4e9b649b603f00c5eb27d9884a870aade085a8ae
Guilherme Basílio <guilhermeqbs@gmail.com>
2020-10-30 17:56:22 -0300

    first commit

---
+++ deadlock/src/main/java/deadlock/ManipuladorArquivo.java
@@ -0,0 +1,40 @@
+package deadlock;
+
+import java.io.*;
+
+
+public class ManipuladorArquivo {
+
+	public static void leitor(String path) throws IOException {
+        
+        File arquivo = new File(path);
+            
+        FileReader fileRead = new FileReader( arquivo );
+        
+        BufferedReader buffRead = new BufferedReader(fileRead);
+        
+        String linha = "";
+        
+		while (true) {
+			if (linha != null) {
+                
+                //Alocamento.listaStr.add(linha);
+                
+			} else
+				break;
+			linha = buffRead.readLine();
+		}
+        buffRead.close();
+        fileRead.close();
+	}
+ 
+
+	public static void escritor(String path, int []mf) throws IOException {
+		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
+        for(int i=0; i<mf.length; i++){
+            buffWrite.append(mf[i] + " ");
+        }
+        buffWrite.close();
+	}
+
+}
---
+++ deadlock/src/main/java/deadlock/Principal.java
@@ -0,0 +1,13 @@
+package deadlock;
+
+/**
+ * Hello world!
+ *
+ */
+public class Principal
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worlqd!" );
+    }
+}
